77e3fa10ce1f85ce442ad63bcfda1fd0071a41bf
Joao Marcelo <joao.mm.santos@outlook.com>
2024-01-26 15:25:47 -0300

    delete

--- jdbcCRUD/src/application/Program.java
+++ jdbcCRUD/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -17,20 +18,19 @@ public class Program {
 			
 			conn = DB.getConnection();
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
+					+ "Id = ?");
 			
-			st.setDouble(1, 200);
-			st.setInt(2, 2);
+			st.setInt(1, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);
 			
-		} catch (SQLException e) {
-			e.getMessage();
+		} 
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
