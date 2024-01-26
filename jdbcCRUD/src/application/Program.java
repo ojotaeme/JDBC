26684b6fa0ff7523a298fb860147a0bbad67279c
Joao Marcelo <joao.mm.santos@outlook.com>
2024-01-26 15:18:42 -0300

    update

--- jdbcCRUD/src/application/Program.java
+++ jdbcCRUD/src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -14,40 +10,29 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		Connection conn = null;
 		PreparedStatement st = null;
-
+		
 		try {
+			
 			conn = DB.getConnection();
-			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl2email.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/2000").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+			
+			st.setDouble(1, 200);
+			st.setInt(2, 2);
+			
 			int rowsAffected = st.executeUpdate();
-
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! ID: " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected");
-			}
-
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+			
 		} catch (SQLException e) {
 			e.getMessage();
-		} catch (ParseException e) {
-			e.getMessage();
-		} finally {
+		}
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
